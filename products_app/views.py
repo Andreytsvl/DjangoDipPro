8433b261216661c01068007b0d0c1021140d77c5
unknown <andreytsvl15@gmail.com>
2024-07-26 08:28:12 +0300

    filter checked

--- products_app/views.py
+++ products_app/views.py
@@ -5,16 +5,23 @@ from django.core.paginator import Paginator
 from products_app.models import Products
 # from goods.utils import q_search
 
-def catalog(request, category_id, page=1):
+def catalog(request, category_id):
+    page = request.GET.get('page', 1)
+    on_sale = request.GET.get('on_sale', None)
+    order_by = request.GET.get('order_by', None)
+
     if category_id == 5:
         products_app = Products.objects.all()
     else:
         products_app = get_list_or_404(Products.objects.filter(category__id=category_id)) #ORM -запрос
-        # if not products_app.exists():
-        #     raise Http404()
+
+    if on_sale:
+        products_app = products_app.filter(discount__gt=0)
+    if order_by and order_by !='default':
+        products_app = products_app.order_by(order_by)
 
     paginator = Paginator(products_app, 3)
-    current_page = paginator.page(page)
+    current_page = paginator.page(int(page))
 
     context = {
         "title": "Home - Каталог",
